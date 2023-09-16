8d149429c0f527be0e3cc2f43be9e529b49f61cf
nekidb <nekidb@gmail.com>
2023-09-16 10:26:21 +0300

    rate limiting and tests

--- main.go
+++ main.go
@@ -1,7 +1,76 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"strings"
+	"sync"
+)
+
+type Server struct {
+	limiter *RateLimiter
+}
+
+func NewServer(limiter *RateLimiter) *Server {
+	return &Server{
+		limiter: limiter,
+	}
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ip := r.Header.Get("XForwarded-For")
+	subnet := extractSubnet(ip, 24)
+
+	if s.limiter.IsLimited(subnet) {
+		w.WriteHeader(http.StatusTooManyRequests)
+		w.Write([]byte("Too many requests"))
+		return
+	}
+
+	s.limiter.Increment(subnet)
+
+	w.Write([]byte("Hello, World!"))
+}
+
+type RateLimiter struct {
+	counter map[string]int
+	mu      sync.RWMutex
+}
+
+func NewRateLimiter() *RateLimiter {
+	return &RateLimiter{
+		counter: make(map[string]int),
+	}
+}
+
+func (l *RateLimiter) Increment(subnet string) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	l.counter[subnet]++
+}
+
+func (l *RateLimiter) IsLimited(subnet string) bool {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+
+	if l.counter[subnet] >= 100 {
+		return true
+	}
+
+	return false
+}
+
+func extractSubnet(ip string, prefixSizeInBits int) string {
+	prefixSizeInBytes := prefixSizeInBits / 8
+
+	parts := strings.Split(ip, ".")
+
+	return strings.Join(parts[:prefixSizeInBytes], ".")
+}
 
 func main() {
-	fmt.Println("vim-go")
+	l := NewRateLimiter()
+	s := NewServer(l)
+
+	log.Fatal(http.ListenAndServe(":8080", s))
 }
---
+++ main_test.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRateLimiter(t *testing.T) {
+	limiter := NewRateLimiter()
+
+	subnet := "123.123.0"
+
+	if limiter.IsLimited(subnet) {
+		t.Fatal("Has not to be limited")
+	}
+
+	for i := 0; i < 100; i++ {
+		limiter.Increment(subnet)
+	}
+
+	if !limiter.IsLimited(subnet) {
+		t.Fatal("Has to be limited")
+	}
+}
+
+func TestServeHTTP(t *testing.T) {
+	limiter := NewRateLimiter()
+	server := NewServer(limiter)
+
+	t.Run("returns OK when not limited", func(t *testing.T) {
+		request := createRequest("123.123.0.1")
+		recorder := httptest.NewRecorder()
+
+		server.ServeHTTP(recorder, request)
+
+		assert.Equal(t, http.StatusOK, recorder.Code)
+		assert.Equal(t, "Hello, World!", recorder.Body.String())
+	})
+
+	t.Run("returns error when limited", func(t *testing.T) {
+		recorder := httptest.NewRecorder()
+
+		request := createRequest("123.45.67.89")
+		for i := 0; i < 32; i++ {
+			server.ServeHTTP(recorder, request)
+		}
+
+		request = createRequest("123.45.67.1")
+		for i := 0; i < 68; i++ {
+			server.ServeHTTP(recorder, request)
+		}
+
+		recorder = httptest.NewRecorder()
+		request = createRequest("123.45.67.111")
+
+		server.ServeHTTP(recorder, request)
+
+		assert.Equal(t, http.StatusTooManyRequests, recorder.Code)
+		assert.Equal(t, "Too many requests", recorder.Body.String())
+	})
+}
+
+func createRequest(ip string) *http.Request {
+	request := httptest.NewRequest("GET", "localhost:8080", nil)
+	request.Header.Add("XForwarded-For", ip)
+
+	return request
+}
