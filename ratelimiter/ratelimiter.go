778225126649db23e602256e4a9c70aaaaa1ced5
nekidb <nekidb@gmail.com>
2023-09-16 12:17:58 +0300

    refactor and add comments

--- ratelimiter/ratelimiter.go
+++ ratelimiter/ratelimiter.go
@@ -7,12 +7,12 @@ import (
 )
 
 type RateLimiter struct {
-	counter    map[string]int
-	lastHits   map[string]time.Time
-	prefixSize int
-	limit      int
-	cooldown   time.Duration
-	mu         sync.RWMutex
+	counter    map[string]int       // количество выполненных запросов из подсети
+	lastHits   map[string]time.Time // время последнего запроса из подсети
+	prefixSize int                  // размер префикса, считываемый из ip адреса
+	limit      int                  // максимальное количество запросов до выставления лимита
+	cooldown   time.Duration        // время, после которого лимит сбрасывается
+	mu         sync.RWMutex         // мьютекс для потокобезопасности мапы
 }
 
 func NewRateLimiter(prefixSize int, limit int, cooldown time.Duration) *RateLimiter {
@@ -25,12 +25,15 @@ func NewRateLimiter(prefixSize int, limit int, cooldown time.Duration) *RateLimi
 	}
 }
 
+// Функция увеличивает счетчик и обновляет время последнего запроса для подсети переданного ip
 func (l *RateLimiter) Increment(ip string) {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 
+	// Вытаскиваем подсеть из ip
 	subnet := extractSubnet(ip, l.prefixSize)
 
+	// Если время кулдауна прошло, то сбрасываем счетчик
 	if lastHit, ok := l.lastHits[subnet]; ok && time.Since(lastHit) > l.cooldown {
 		l.counter[subnet] = 0
 	}
@@ -39,16 +42,20 @@ func (l *RateLimiter) Increment(ip string) {
 	l.lastHits[subnet] = time.Now()
 }
 
+// Функция проверяет, выставлен ли лимит для подсети переданного ip
 func (l *RateLimiter) IsLimited(ip string) bool {
 	l.mu.RLock()
 	defer l.mu.RUnlock()
 
+	// Вытаскиваем подсеть из ip
 	subnet := extractSubnet(ip, l.prefixSize)
 
+	// Если время кулдауна прошло, то лимита нет
 	if lastHit, ok := l.lastHits[subnet]; ok && time.Since(lastHit) > l.cooldown {
 		return false
 	}
 
+	// Если количество запросов для подсети превышает число лимита, то лимит выставлен на него
 	if l.counter[subnet] >= l.limit {
 		return true
 	}
@@ -56,6 +63,7 @@ func (l *RateLimiter) IsLimited(ip string) bool {
 	return false
 }
 
+// Функция сбрасывает лимит для подсети
 func (l *RateLimiter) Reset(subnet string) {
 	l.mu.RLock()
 	defer l.mu.RUnlock()
@@ -64,7 +72,9 @@ func (l *RateLimiter) Reset(subnet string) {
 	delete(l.lastHits, subnet)
 }
 
+// Функция вытаскивает подсеть из ip по размеру префикса
 func extractSubnet(ip string, prefixSizeInBits int) string {
+	// Для упрощения предполагается, что подсеть состоит из полных чисел представленных в ip
 	prefixSizeInBytes := prefixSizeInBits / 8
 
 	parts := strings.Split(ip, ".")
--- server/server.go
+++ server/server.go
@@ -1,67 +1,74 @@
 package server
 
 import (
-	"log"
+	"fmt"
+	"io"
 	"net/http"
 )
 
+// Итерфейс лимитера используется для возможности простой замены лимитера, и чтобы можно было мокать
 type Limiter interface {
 	Increment(ip string)
 	IsLimited(ip string) bool
 	Reset(subnet string)
 }
 
+// Структура сервера содержит роутер для маршрутизации и лимитер для ограчения
 type Server struct {
 	router  *http.ServeMux
 	limiter Limiter
 }
 
 func NewServer(limiter Limiter) *Server {
+	// Создаем роутер, сервер
 	router := http.NewServeMux()
-
 	server := &Server{
 		router:  router,
 		limiter: limiter,
 	}
 
+	// Регистрируем хендлеры
 	router.HandleFunc("/", server.defaultHandler)
 	router.HandleFunc("/reset", server.resetHandler)
 
 	return server
 }
 
+// Хендлер обрабатывает обычные запросы к серверу
 func (s Server) defaultHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Serving HTTP")
 	ip := r.Header.Get("XForwarded-For")
 
+	// Если для подсети данного ip превышен лимит запросов, то возвращаем "429 Too Many Requests"
 	if s.limiter.IsLimited(ip) {
-		log.Println("limited hit")
 		w.WriteHeader(http.StatusTooManyRequests)
 		w.Write([]byte("Too Many Requests"))
 		return
 	}
 
+	// Если лимит не превышен, то увеличиваем счетик лимитера и выводим статический контент
 	s.limiter.Increment(ip)
 
 	w.Write([]byte("Hello, World!"))
 
 }
 
+// Хендлер обрабатывает запросы на сброс лимита по префиксу
 func (s Server) resetHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Serving HTTP")
-	ip := r.Header.Get("XForwarded-For")
-
-	if s.limiter.IsLimited(ip) {
-		log.Println("limited hit")
-		w.WriteHeader(http.StatusTooManyRequests)
-		w.Write([]byte("Too Many Requests"))
+	// Вытаскиваем префикс из тела запроса
+	subnetBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal server error"))
 		return
 	}
+	defer r.Body.Close()
 
-	s.limiter.Increment(ip)
+	subnet := string(subnetBytes)
 
-	w.Write([]byte("Hello, World!"))
+	// Сбрасываем для префикса его лимит
+	s.limiter.Reset(subnet)
 
+	w.Write([]byte(fmt.Sprintf("Limit for %s was reseted", subnet)))
 }
 
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
